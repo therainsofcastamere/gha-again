2f3af78132e447e1d9716e6283dce28ce908c9d3
Rajendra Maruthi Mahawadi <12994577+therainsofcastamere@users.noreply.github.com>
2024-08-05 18:36:16 +1200

    first actual commit

---
+++ app/src/test/java/com/mango/practical/gitops/GitopsApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.mango.practical.gitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
